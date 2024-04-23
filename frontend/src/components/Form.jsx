70ea880a3c68af4afabffc23b72eeac78a756785
Klajd Arqimandriti <klajd.80@gmail.com>
2024-04-23 15:44:36 +0200

    Updated Form jsx file

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -18,6 +18,7 @@ function Form({ route, method }) {
         e.preventDefault();
 
         try {
+            console.log(`Route: ${route}`)
             const res = await api.post(route, { username, password })
             if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);

915521be9ad4c9946a8787fdbb6ebfb905e2fe83
Klajd Arqimandriti <klajd.80@gmail.com>
2024-05-10 13:44:28 +0200

    Re-added all filles are temporary purge

---
+++ backend/api/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Tire, Transaction, ReservedTire
+# from .models import HotelTire
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+
+class TireSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tire
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class ReservedTireSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReservedTire
+        fields = '__all__'
+
+# class HotelTireSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = HotelTire
+#         fields = '__all__'
---
+++ backend/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("tires/", views.TireList.as_view(), name="tire-list"),
+    path("tires/delete/<int:pk>", views.TireDelete.as_view(), name="delete-tire"),
+    path("filter/", views.filter_tires, name="filter_tires"),
+    path("reserved_tires/", views.get_reserved_tires, name="get_reserved_tires"),
+    
+    path('sellTire/<int:tire_id>/', views.sell_tire, name='sell_tire'),
+    path('addTireStock/<int:tire_id>/', views.add_tire_stock, name='add_tire_stock'),
+
+    path('reserveTire/<int:tire_id>/', views.reserve_tire, name='reserve_tire'),
+    path('unReserveTire/<int:tire_id>/', views.unreserve_tire, name='unreserve_tire'),
+]

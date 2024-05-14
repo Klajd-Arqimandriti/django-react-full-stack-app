3cf354b665a9755103b1b62fcd1c84f820a41c41
Klajd Arqimandriti <klajd.80@gmail.com>
2024-05-14 11:53:59 +0200

    Added Choreo backend api route

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,13 +2,9 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
  
-// const apiUrl = "/choreo-apis/djangoreactapp/backend/rest-api-be2/v1.0"
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
-// })
-
+const apiUrl = "/choreo-apis/protirewebapp/backend/rest-api-be2/v1"
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(

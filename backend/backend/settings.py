ef9483a21a51434186b6d64f45dce919a186de67
Klajd Arqimandriti <klajd.80@gmail.com>
2024-05-15 11:08:56 +0200

    Changed secret key retrieval method

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-37(pnv52t(+g1(tee*ro1f-#54zjmgo#-^ugkz9=-6pm^5%6$8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
